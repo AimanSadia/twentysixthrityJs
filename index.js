7304597832d5390c5ad822d70d61f02580d49ed7
Aiman Sadia <102213880+AimanSadia@users.noreply.github.com>
2023-07-04 06:44:15 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+
+
+
+/* QUESTION 01 */
+
+/* var num = parseFloat(prompt("Enter your positive decimal value:"));
+
+var roundValue = Math.round(num);
+var floorValue = Math.floor(num);
+var ceilValue = Math.ceil(num);
+
+document.write("Number: " + num + "<br>");
+document.write("Round off value: " + roundValue + "<br>");
+document.write("Floor value: " + floorValue + "<br>");
+document.write("Ceil value: " + ceilValue + "<br>"); */
+
+/* QUESTION 02 */
+
+/* var num = parseFloat(prompt("Enter your negative decimal value:"));
+
+var roundValue = Math.round(num);
+var floorValue = Math.floor(num);
+var ceilValue = Math.ceil(num);
+
+document.write("Number: " + num + "<br>");
+document.write("Round off value: " + roundValue + "<br>");
+document.write("Floor value: " + floorValue + "<br>");
+document.write("Ceil value: " + ceilValue + "<br>"); */
+
+
+/* QUESTION 03 */
+
+/* var number = -4;
+
+var absoluteValue;
+
+if (number < 0) {
+    absoluteValue = -number; 
+} else {
+    absoluteValue = number; 0
+}
+
+document.write(" The absolute value of -4 is: " + absoluteValue); */
+
+/* QUESTION 04 */
+
+/* 
+var diceVal = Math.floor(Math.random() * 50) + 1;
+document.write("Random dice Value: " + diceVal); */
+
+/* QUESTION 05 */
+
+/* var ranNum = Math.random();
+
+var coin;
+
+if (ranNum < 0.5) {
+    coin = "Heads";
+} else {
+    coin = "Tails";
+}
+
+document.write("Random coin value: " + coin); */
+
+
+/* QUESTION 06 */
+
+/* var ranNumber = Math.floor(Math.random() * 100) + 1;
+
+document.write("Random Number between 1 and 100: " + ranNumber); */
+
+
+/* QUESTION 07 */
+
+/* var userWeight = prompt("Enter your weight:");
+
+var weight = parseFloat(userWeight);
+
+document.write("The weight of user is: " + weight + " kilograms"); */
+
+
+/* QUESTION 08 */
+
+/* var x = Math.floor(Math.random() * 10) + 1;
+
+var userInput = parseInt(prompt("Enter a number between 1 and 10:"));
+
+if (userInput === x) {
+    document.write("Congratulations! You guessed the secret number.");
+} else {
+    document.write("Sorry, your guess is incorrect. The secret number was " + x);
+} */
